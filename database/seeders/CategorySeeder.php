e8c8a2652737c0c4f5dc051b496cad3f13c77e5c
Richard Marcell <richard.marcell8888@gmail.com>
2025-09-17 20:05:37 +0700

    fix: seeder error with wrong class

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CategoriesTableSeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     public function run()
     {
